69592efe495599e0fc75a452530e8f4c674217e3
raidan PC <raidan00@mail.ru>
2024-05-01 15:39:00 +0300

    move dvijcock to root

---
+++ dvijcock/README.md
@@ -0,0 +1,22 @@
+## Dvijcock
+3D web game engine that use Svelte + Three.js + Ammo.js. <br/>
+Name derived from russian word "движкок" that mean engine. <br/>
+In source code you will often see short version of name dvijcock - dc.
+## Warn
+Dvijcock in active phase of arcitecture and API development. Be caraful with Dvijcock versions. API can be changed every version.
+## Learn
+For using Dvijcock you need to know JavaScript, Svelte, Three.js and Ammo.js<br/>
+* As start point learn source code with comments this simplest Dvijcock example<br/>
+Live - https://raidan00.github.io/hello-dvijcock/dist/index.html<br/>
+Source - https://github.com/raidan00/hello-dvijcock
+* Watch video tutorial simplest game created in Blender and imported to Dvijcock<br/>
+Video Tutorial - https://youtu.be/KsdXPuISbw8?si=2TuCNKBw7si223qJ<br/>
+Source - https://github.com/raidan00/hello-dvijcock-blender
+## Advanced learn
+Currently I am participate in ithc.io jams and after submissions is closed I publish games source code in my GitHub repositories. Game repository name starts with "dc-game-..."<br/>
+Examples:<br/>
+* Power and Influence<br/>
+Live - https://wipefire.itch.io/power-and-influence<br/>
+Source - https://github.com/raidan00/dc-game-power-and-influence<br/>
+
+
---
+++ dvijcock/moveController/MoveController.js
@@ -0,0 +1,47 @@
+import * as t from 'three';
+import moveDirection from './moveDirection.js';
+import MoveControler from './MoveControler.svelte'
+import ammoTmp from "dvijcock/ammoTmp.js"
+
+//controls is OrbitControls or Camera
+export default class {
+	constructor(objThree, controls, pushForce, maxSpeed){ 
+		pushForce *= 10;
+		objThree.dcData.rbody.setActivationState(4)
+		function tickBeforePhysics(){
+			if(moveDirection.forward == 0 && moveDirection.right  == 0) return;
+			if(controls.getAzimuthalAngle){
+				var pushAng = moveDirection.angle - controls.getAzimuthalAngle() - Math.PI/2;
+			}else{
+				var pushAng = moveDirection.angle - Math.PI +
+					new t.Vector2(controls.position.x, controls.position.z).angle();
+			}
+			var pushVec = new t.Vector2( Math.cos(pushAng), Math.sin(pushAng));
+			let velocity = objThree.dcData.rbody.getLinearVelocity();
+			let velVec = new t.Vector2(velocity.x(), velocity.z());
+			if(velVec.length()>maxSpeed && pushVec.angleTo(velVec) < Math.PI/2){
+				let velAngle = velVec.angle();
+				let perpendicular = velAngle + Math.PI/2 * Math.sign(velAngle-pushAng);
+				pushVec = new t.Vector2( Math.cos(perpendicular), Math.sin(perpendicular) );
+				pushVec.multiplyScalar(Math.cos(pushAng-perpendicular));
+			};
+			pushVec.multiplyScalar(pushForce*moveDirection.touchFactor);
+			objThree.dcData.rbody.applyCentralForce(ammoTmp.vec(pushVec.x, 0, pushVec.y));
+		}
+		if(!objThree.dcData.tickBeforePhysics){
+			objThree.dcData.tickBeforePhysics = tickBeforePhysics;
+		}else{
+			objThree.dcData.tickBeforePhysics = [objThree.dcData.tickBeforePhysics, tickBeforePhysics];
+		}
+		this.div = document.createElement('div');
+		this.div.className = 'dvijcock-conroller';
+		document.body.appendChild(this.div);
+		this.app = new MoveControler({
+			target: this.div,
+		})
+	}
+	destroy(){
+		this.app.$destroy();
+		this.div.remove();
+	}
+}
