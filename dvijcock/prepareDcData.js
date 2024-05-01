888a0abb7efd6032342a52217f67fe52d4716819
raidan PC <raidan00@mail.ru>
2024-05-02 00:47:53 +0300

    save resurces

--- dvijcock/prepareDcData.js
+++ dvijcock/prepareDcData.js
@@ -41,14 +41,11 @@ export default function(objThree){
 			var mesh = new Ammo.btTriangleMesh(true, true);
 			let index = objThree.geometry.index.array;
 			let vert = objThree.geometry.attributes.position.array;
-			console.log(index, vert);
-			for (let i = 0; i < index.length; i += 3 ) {
-				mesh.addTriangle(
-					new Ammo.btVector3(vert[index[i+0]*3+0], vert[index[i+0]*3+1], vert[index[i+0]*3+2]),
-					new Ammo.btVector3(vert[index[i+1]*3+0], vert[index[i+1]*3+1], vert[index[i+1]*3+2]),
-					new Ammo.btVector3(vert[index[i+2]*3+0], vert[index[i+2]*3+1], vert[index[i+2]*3+2]),
-					false
-				);
+			for (let i = 0; i < index.length; i+=3){
+				ammoTmp.vecArr[0].setValue(vert[index[i+0]*3+0], vert[index[i+0]*3+1], vert[index[i+0]*3+2]);
+				ammoTmp.vecArr[1].setValue(vert[index[i+1]*3+0], vert[index[i+1]*3+1], vert[index[i+1]*3+2]);
+				ammoTmp.vecArr[2].setValue(vert[index[i+2]*3+0], vert[index[i+2]*3+1], vert[index[i+2]*3+2]);
+				mesh.addTriangle(ammoTmp.vecArr[0], ammoTmp.vecArr[1], ammoTmp.vecArr[2], false); 
 			}
 			objThree.dcData.btShape = new Ammo.btBvhTriangleMeshShape(mesh, true, true);
 		}
