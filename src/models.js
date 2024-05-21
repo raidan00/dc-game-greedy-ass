6db2cd39ae3867c1d82dc85100318686dc6db314
raidan PC <raidan00@mail.ru>
2024-05-21 14:01:54 +0300

    bugfix

--- src/models.js
+++ src/models.js
@@ -11,7 +11,11 @@ const loader = new GLTFLoader();
 async function loadModel(val){
 	let url = await val();
 	let model = await loader.loadAsync(url);
-	models[url.match(/([A-Za-z0-9_.-]+)\.glb+/)[1]] = model;
+	if(import.meta.env.MODE == "development"){
+		models[url.match(/([A-Za-z0-9_.-]+)\.glb+/)[1]] = model;
+	}else{
+		models[url.match(/([A-Za-z0-9_.-]+)-.+\.glb+/)[1]] = model;
+	}
 }
 export function loadModels(){
 	let load = Object.values(urls).map(val=>loadModel(val));
